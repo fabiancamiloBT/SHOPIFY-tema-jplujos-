7a16c8273d1d92c1040b356be000777752b8dc56
fabiancamiloBT <fabianbarbosa@usantotomas.edu.co>
2026-09-01 14:13:40 -0500

    Agregar carpeta assets (CSS, JS, imagenes) al tema

---
+++ assets/gem-page-product-1685718634.js
@@ -0,0 +1,194 @@
+
+
+    (function( jQuery ){
+  var $module = jQuery('#m-1683726385714').children('.module');
+  $module.gfV3Product();
+})( window.GemQuery || jQuery );
+  
+    
+  
+    (function(jQuery) {
+  var $module = jQuery('#m-1683726388221').children('.module');
+  var effect = $module.attr('data-effect');
+  var magnify = $module.attr('data-zoom-level');
+  var displayType = $module.attr('data-displaytype');
+  $module.gfV3ProductImage({
+    'effect': effect,
+    'displayType': displayType,
+    'magnify': magnify
+  });
+})(window.GemQuery || jQuery);
+  
+    (function(jQuery) {
+  var $module = jQuery('#m-1686005022332').children('.module');
+  $module.gfV4ProductImageList({
+    onImageClicked: function(imageUrl, imageZoomUrl) {}
+  });
+
+  var style = $module.attr('data-style');
+  switch(style) {
+    case 'slider':
+    var navspeed = $module.data('navspeed'),
+      navlg = $module.data('navlg'),
+      navmd = $module.data('navmd'),
+      navsm = $module.data('navsm'),
+      navxs = $module.data('navxs'),
+      collg = $module.data('collg'),
+      colmd = $module.data('colmd'),
+      colsm = $module.data('colsm'),
+      colxs = $module.data('colxs'),
+      dotslg = $module.data('dotslg'),
+      dotsmd = $module.data('dotsmd'),
+      dotssm = $module.data('dotssm'),
+      dotsxs = $module.data('dotsxs'),
+
+      marginlg = parseInt($module.data('mlg')),
+      marginmd = parseInt($module.data('mmd')),
+      marginsm = parseInt($module.data('msm')),
+      marginxs = parseInt($module.data('mxs'));
+
+    var mode = jQuery('.gryffeditor').hasClass('editing') ? 'dev' : 'production';
+    if(mode == 'production') {
+      var loop = $module.data('loop'),
+        autoRefresh = true;
+    } else {
+      var loop = 0,
+        autoRefresh = false;
+    }
+    $module.find('.gf_product-images-list').children().each( function( index ) {
+      jQuery(this).attr( 'data-position', index );
+    });
+    $module.find('.gf_product-images-list').owlCarousel({
+      mouseDrag: false,
+      navSpeed: navspeed,
+      autoRefresh: autoRefresh,
+      autoWidth: !1,
+      loop: loop,
+      responsiveClass:true,
+      responsive:{
+        0:{
+          items:colxs,
+          nav: navxs,
+          dots:dotsxs,
+          margin: marginxs
+        },
+        768:{
+          items:colsm,
+          nav: navsm,
+          dots:dotssm,
+          margin: marginsm
+        },
+        992:{
+          items:colmd,
+          nav: navmd,
+          dots:dotsmd,
+          margin: marginmd
+        },
+        1200:{
+          items:collg,
+          nav: navlg,
+          dots:dotslg,
+          margin: marginlg
+        }
+      },
+      onInitialized: function(){
+        $module.closest('.module-wrap[data-label="(P) Image List"]').addClass('gf-carousel-loaded');
+      }
+    }); 
+    break;
+  }
+})(window.GemQuery || jQuery); 
+  
+    (function(jQuery) {
+    var $module = jQuery('#m-1683726387484').children('.module');
+    $module.gfV3ProductPrice({
+        displayCurrency: true
+    });
+})(window.GemQuery || jQuery);
+  
+    
+  
+    (function( jQuery ){
+    var $module = jQuery('#m-1683729662197').children('.module');
+    $module.gfV1Countdown({
+        id: "1683729662197",
+        idSlug: "1683729662197"
+    });
+})( window.GemQuery || jQuery );
+  
+    (function(jQuery) {
+    var $module = jQuery('#m-1685546451892').children('.module');
+    $module.gfV3ProductPrice({
+        displayCurrency: true
+    });
+})(window.GemQuery || jQuery);
+  
+    
+  window.__gfAnimate.registerScroll(jQuery("#e-1685112808944"), '{"animation":{"screens":{"lg":true,"md":true,"sm":true,"xs":true},"name":"rubber-band","delay":0,"duration":500,"iteration-count":"1"},"interaction":{"key":"scroll-into-view","type":"level2"}}');
+    
+  window.__gfAnimate.registerScrolling(jQuery("#e-1685488432494"), '{"animation":{"screens":{"lg":true,"md":true,"sm":true,"xs":true},"name":"flash","delay":0,"duration":1300,"iteration-count":"infinite"},"interaction":{"key":"scrolling-in-view","type":"level2"}}');
+    
+  
+    
+  
+    
+  
+    
+  window.__gfAnimate.registerScroll(jQuery("#e-1683736146245"), '{"animation":{"screens":{"lg":true,"md":true,"sm":true,"xs":true},"name":"rubber-band","delay":0,"duration":500,"iteration-count":"1"},"interaction":{"key":"scroll-into-view","type":"level2"}}');
+    (function( jQuery ){
+  var $module = jQuery('#m-1685109548031').children('.module');
+  $module.gfV1StockCounter();
+})( window.GemQuery || jQuery );
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  window.__gfAnimate.registerScroll(jQuery("#e-1683736392137"), '{"animation":{"screens":{"lg":true,"md":true,"sm":true,"xs":true},"name":"rubber-band","delay":0,"duration":500,"iteration-count":"1"},"interaction":{"key":"scroll-into-view","type":"level2"}}');
+    
+  
+    
+  
+    
+  
+    
+  window.__gfAnimate.registerScroll(jQuery("#e-1685116786498"), '{"animation":{"screens":{"lg":true,"md":true,"sm":true,"xs":true},"name":"rubber-band","delay":0,"duration":500,"iteration-count":"1"},"interaction":{"key":"scroll-into-view","type":"level2"}}');
+    
+  
+    
+  
+    
+  
+    
+  window.__gfAnimate.registerScroll(jQuery("#e-1683736391334"), '{"animation":{"screens":{"lg":true,"md":true,"sm":true,"xs":true},"name":"rubber-band","delay":0,"duration":500,"iteration-count":"1"},"interaction":{"key":"scroll-into-view","type":"level2"}}');
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  window.__gfAnimate.registerScroll(jQuery("#e-1685723235271"), '{"animation":{"screens":{"lg":true,"md":true,"sm":true,"xs":true},"name":"rubber-band","delay":0,"duration":500,"iteration-count":"1"},"interaction":{"key":"scroll-into-view","type":"level2"}}');
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
